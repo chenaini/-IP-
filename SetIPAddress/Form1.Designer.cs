628358d295aba6abd0abcae8611a4bf4b9d9c8c3
csw <chenshiwang1314@163.com>
2017-08-24 09:44:01 +0800

    原版，初始化基本功能。

---
+++ SetIPAddress/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace SetIPAddress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.txtNetwork = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.txtName = new DevExpress.XtraEditors.TextEdit();
+            this.txtDNS = new DevExpress.XtraEditors.TextEdit();
+            this.txtGetway = new DevExpress.XtraEditors.TextEdit();
+            this.txtSubmask = new DevExpress.XtraEditors.TextEdit();
+            this.txtIP = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.删除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn4 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn5 = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNetwork.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtName.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDNS.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGetway.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSubmask.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtIP.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(35, 68);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(47, 14);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "IP地址：";
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.txtNetwork);
+            this.groupControl1.Controls.Add(this.simpleButton2);
+            this.groupControl1.Controls.Add(this.simpleButton1);
+            this.groupControl1.Controls.Add(this.labelControl2);
+            this.groupControl1.Controls.Add(this.txtName);
+            this.groupControl1.Controls.Add(this.txtDNS);
+            this.groupControl1.Controls.Add(this.txtGetway);
+            this.groupControl1.Controls.Add(this.txtSubmask);
+            this.groupControl1.Controls.Add(this.txtIP);
+            this.groupControl1.Controls.Add(this.labelControl5);
+            this.groupControl1.Controls.Add(this.labelControl4);
+            this.groupControl1.Controls.Add(this.labelControl3);
+            this.groupControl1.Controls.Add(this.labelControl1);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupControl1.Location = new System.Drawing.Point(0, 0);
+            this.groupControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(640, 194);
+            this.groupControl1.TabIndex = 1;
+            this.groupControl1.Text = "IP地址信息";
+            // 
+            // txtNetwork
+            // 
+            this.txtNetwork.Location = new System.Drawing.Point(330, 34);
+            this.txtNetwork.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNetwork.Name = "txtNetwork";
+            this.txtNetwork.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.txtNetwork.Properties.TextEditStyle = DevExpress.XtraEditors.Controls.TextEditStyles.DisableTextEditor;
+            this.txtNetwork.Size = new System.Drawing.Size(272, 20);
+            this.txtNetwork.TabIndex = 4;
+            this.txtNetwork.SelectedIndexChanged += new System.EventHandler(this.txtNetwork_SelectedIndexChanged);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(449, 76);
+            this.simpleButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(69, 62);
+            this.simpleButton2.TabIndex = 3;
+            this.simpleButton2.Text = "自动获取";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(258, 76);
+            this.simpleButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(69, 62);
+            this.simpleButton1.TabIndex = 3;
+            this.simpleButton1.Text = "保存";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(23, 36);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(60, 14);
+            this.labelControl2.TabIndex = 2;
+            this.labelControl2.Text = "保存名称：";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(84, 31);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(131, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtDNS
+            // 
+            this.txtDNS.Location = new System.Drawing.Point(84, 161);
+            this.txtDNS.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDNS.Name = "txtDNS";
+            this.txtDNS.Size = new System.Drawing.Size(131, 20);
+            this.txtDNS.TabIndex = 1;
+            // 
+            // txtGetway
+            // 
+            this.txtGetway.Location = new System.Drawing.Point(84, 126);
+            this.txtGetway.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtGetway.Name = "txtGetway";
+            this.txtGetway.Size = new System.Drawing.Size(131, 20);
+            this.txtGetway.TabIndex = 1;
+            // 
+            // txtSubmask
+            // 
+            this.txtSubmask.EditValue = "255.255.255.0";
+            this.txtSubmask.Location = new System.Drawing.Point(84, 98);
+            this.txtSubmask.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSubmask.Name = "txtSubmask";
+            this.txtSubmask.Size = new System.Drawing.Size(131, 20);
+            this.txtSubmask.TabIndex = 1;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(84, 66);
+            this.txtIP.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(131, 20);
+            this.txtIP.TabIndex = 1;
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Location = new System.Drawing.Point(47, 163);
+            this.labelControl5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(35, 14);
+            this.labelControl5.TabIndex = 0;
+            this.labelControl5.Text = "DNS：";
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Location = new System.Drawing.Point(46, 131);
+            this.labelControl4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(36, 14);
+            this.labelControl4.TabIndex = 0;
+            this.labelControl4.Text = "网关：";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(23, 100);
+            this.labelControl3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(60, 14);
+            this.labelControl3.TabIndex = 0;
+            this.labelControl3.Text = "子网掩码：";
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.ContextMenuStrip = this.contextMenuStrip1;
+            this.gridControl1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gridControl1.Location = new System.Drawing.Point(0, 194);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(640, 281);
+            this.gridControl1.TabIndex = 4;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            this.gridControl1.DoubleClick += new System.EventHandler(this.gridControl1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.删除ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            // 
+            // 删除ToolStripMenuItem
+            // 
+            this.删除ToolStripMenuItem.Name = "删除ToolStripMenuItem";
+            this.删除ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.删除ToolStripMenuItem.Text = "删除";
+            this.删除ToolStripMenuItem.Click += new System.EventHandler(this.删除ToolStripMenuItem_Click);
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumn1,
+            this.gridColumn2,
+            this.gridColumn4,
+            this.gridColumn3,
+            this.gridColumn5});
+            this.gridView1.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsBehavior.Editable = false;
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.Caption = "名称";
+            this.gridColumn1.FieldName = "Name";
+            this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.Visible = true;
+            this.gridColumn1.VisibleIndex = 0;
+            // 
+            // gridColumn2
+            // 
+            this.gridColumn2.Caption = "IP地址";
+            this.gridColumn2.FieldName = "IP";
+            this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.Visible = true;
+            this.gridColumn2.VisibleIndex = 1;
+            // 
+            // gridColumn3
+            // 
+            this.gridColumn3.Caption = "网关";
+            this.gridColumn3.FieldName = "Getway";
+            this.gridColumn3.Name = "gridColumn3";
+            this.gridColumn3.Visible = true;
+            this.gridColumn3.VisibleIndex = 3;
+            // 
+            // gridColumn4
+            // 
+            this.gridColumn4.Caption = "子网掩码";
+            this.gridColumn4.FieldName = "Submask";
+            this.gridColumn4.Name = "gridColumn4";
+            this.gridColumn4.Visible = true;
+            this.gridColumn4.VisibleIndex = 2;
+            // 
+            // gridColumn5
+            // 
+            this.gridColumn5.Caption = "DNS";
+            this.gridColumn5.FieldName = "DNS";
+            this.gridColumn5.Name = "gridColumn5";
+            this.gridColumn5.Visible = true;
+            this.gridColumn5.VisibleIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 475);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.groupControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "设置本机IP地址";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.groupControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNetwork.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtName.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDNS.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGetway.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSubmask.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtIP.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.TextEdit txtIP;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.TextEdit txtName;
+        private DevExpress.XtraEditors.TextEdit txtDNS;
+        private DevExpress.XtraEditors.TextEdit txtGetway;
+        private DevExpress.XtraEditors.TextEdit txtSubmask;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn4;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn5;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 删除ToolStripMenuItem;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.ComboBoxEdit txtNetwork;
+    }
+}
+
---
+++ SetIPAddress/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SetIPAddress
+{
+    public partial class Form1 : Form
+    {
+        string ipfile = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadData()
+        {
+            if (File.Exists(ipfile))
+            {
+                DataSet ds = new DataSet();
+                ds.ReadXml(ipfile, XmlReadMode.ReadSchema);
+                gridControl1.DataSource = ds.Tables[0].DefaultView;
+            }
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if(txtName.Text=="" || txtIP.Text=="" || txtGetway.Text=="" || txtSubmask.Text=="")
+            {
+                MessageBox.Show("设置错误！");
+            }
+            if(File.Exists(ipfile))
+            {
+                DataSet ds = new DataSet();
+                ds.ReadXml(ipfile, XmlReadMode.ReadSchema);
+                DataRow row = ds.Tables[0].NewRow();
+                row["Name"] = txtName.Text;
+                row["IP"] = txtIP.Text;
+                row["Getway"] = txtGetway.Text;
+                row["Submask"] = txtSubmask.Text;
+                row["DNS"] = txtDNS.Text;
+                ds.Tables[0].Rows.Add(row);
+                ds.WriteXml(ipfile, XmlWriteMode.WriteSchema);
+                LoadData();
+                txtDNS.Text = "";
+                txtGetway.Text = "";
+                txtDNS.Text = "";
+                txtIP.Text = "";
+                txtName.Text = "";
+                txtSubmask.Text = "255.255.255.0";
+            }
+            else
+            {
+                DataSet ds = new DataSet();
+                DataTable table = new DataTable();
+                table.Columns.Add("Name");
+                table.Columns.Add("IP");
+                table.Columns.Add("Getway");
+                table.Columns.Add("Submask");
+                table.Columns.Add("DNS");
+
+                DataRow row = table.NewRow();
+                row["Name"] = txtName.Text;
+                row["IP"] = txtIP.Text;
+                row["Getway"] = txtGetway.Text;
+                row["Submask"] = txtSubmask.Text;
+                row["DNS"] = txtDNS.Text;
+                table.Rows.Add(row);
+                ds.Tables.Add(table);
+                ds.WriteXml(ipfile, XmlWriteMode.WriteSchema);
+                LoadData();
+                txtDNS.Text = "";
+                txtGetway.Text = "";
+                txtDNS.Text = "";
+                txtIP.Text = "";
+                txtName.Text = "";
+                txtSubmask.Text = "255.255.255.0";
+            }
+        }
+
+        private void 删除ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(gridView1.FocusedRowHandle>-1)
+            {
+                if (File.Exists(ipfile))
+                {
+                    DataSet ds = new DataSet();
+                    ds.ReadXml(ipfile, XmlReadMode.ReadSchema);
+                    ds.Tables[0].Rows.RemoveAt(gridView1.FocusedRowHandle);
+                    ds.WriteXml(ipfile, XmlWriteMode.WriteSchema);
+                    LoadData();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ipfile = Application.StartupPath + "\\IP.txt";
+            LoadData();
+            //ManagementClass wmi = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            //ManagementObjectCollection moc = wmi.GetInstances();
+            //foreach (ManagementObject mo in moc)
+            //{
+            //    if(mo.Properties["MACAddress"].Value!=null && mo.Properties["MACAddress"].Value.ToString()!="")
+            //    {
+            //        txtNetwork.Properties.Items.Add(mo.Properties["Description"].Value.ToString());
+            //    }
+            //}
+            ManagementClass mc = new ManagementClass("Win32_NetworkAdapter");
+            ManagementObjectCollection moc = mc.GetInstances();
+            foreach (ManagementObject mo in moc)
+            {
+                if (mo["NetConnectionID"] != null)
+                {
+                    txtNetwork.Properties.Items.Add(mo["NetConnectionID"].ToString());
+                }
+            }
+            if(txtNetwork.Properties.Items.Count>0)
+            {
+                txtNetwork.SelectedIndex = 0;
+            }
+        }
+
+        private void gridControl1_DoubleClick(object sender, EventArgs e)
+        {
+            if (txtNetwork.Text == "")
+            {
+                MessageBox.Show("请选择网卡！");
+                return;
+            }
+            if (gridView1.FocusedRowHandle > -1)
+            {
+                string _ipaddress = gridView1.GetFocusedRowCellValue("IP").ToString();
+                string _submask = gridView1.GetFocusedRowCellValue("Submask").ToString();
+                string _gateway = gridView1.GetFocusedRowCellValue("Getway").ToString();
+                string _dns1 = gridView1.GetFocusedRowCellValue("DNS").ToString();
+                //NetworkSetting.SetIPAddress(_ipaddress, _submask, _gateway);
+                //if (_dns1 != "")
+                //{
+                //    NetworkSetting.SetDNS(new string[] { _dns1 });
+                //}
+
+                string _doscmd = "netsh interface ip set address " + txtNetwork.Text + " static " + _ipaddress + " " + _submask + " " + _gateway + " 2";
+                Process p = new Process();
+                p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.CreateNoWindow = true;
+                p.Start();
+                p.StandardInput.WriteLine(_doscmd.ToString());
+                _doscmd = "netsh interface ip set dns " + txtNetwork.Text + " static " + _dns1;
+                p.StandardInput.WriteLine(_doscmd.ToString());
+
+                p.StandardInput.WriteLine("exit");
+
+                MessageBox.Show("设置完成！");
+            }
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            //NetworkSetting.EnableDHCP();
+            if(txtNetwork.Text=="")
+            {
+                MessageBox.Show("请选择网卡！");
+                return;
+            }
+            string _doscmd = "netsh interface ip set address  " + txtNetwork.Text + " DHCP";
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            p.StandardInput.WriteLine(_doscmd.ToString());
+            _doscmd = "netsh interface ip set dns  " + txtNetwork.Text + " DHCP";
+            p.StandardInput.WriteLine(_doscmd.ToString());
+            p.StandardInput.WriteLine("exit");
+
+
+            MessageBox.Show("设置完成！");
+        }
+
+        private void txtNetwork_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            NetworkSetting.NetworkName = txtNetwork.Text;
+        }
+
+
+    }
+}
